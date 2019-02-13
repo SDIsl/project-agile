101c50c9db9cbd7492bc68999918b6914cb677fd
JorgeQuinteros <quinterosorellana04@gmail.com>
2019-02-13 09:35:09 +0100

    [IMP] a few changes in view, and fields

--- scrum_project/models/scrum_sprint.py
+++ scrum_project/models/scrum_sprint.py
@@ -1,11 +1,10 @@
 from odoo import api, fields, models, _
 
 class ScrumSprint (models.Model):
-    _name = 'sprint'
+    _name = 'scrum.sprint'
 
     sprint_name = fields.Char(string='Sprint name')
     sprint_init = fields.Date(string='Sprint Start')
     sprint_end = fields.Date(string='Sprint End')
-    
     
     
