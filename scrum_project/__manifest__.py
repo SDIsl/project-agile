1e5082fe2097db64db56f91e7032f781630bec5e
oscars8a <oscar.soto.ochoa@gmail.com>
2019-02-14 09:04:07 +0100

    [ADD] xml base

--- scrum_project/__manifest__.py
+++ scrum_project/__manifest__.py
@@ -2,18 +2,18 @@
 # © 2019 Jorge Luis Quinteros <jquinteros@sdi.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name':'SCRUM Project',
+    'name': 'SCRUM Project',
     'version': '11.0.1.0.0',
     'category': 'Project Agile',
     'author': 'Jorge Luis Quinteros',
-    'summary': """  
-        Create SCRUM environment""",
+    'summary': """ Create SCRUM environment""",
     'license': 'AGPL-3',
     'depends': [
         'project',
     ],
     'data': [
-
+        'views/scrum_project.xml',
+        'views/scrum_team.xml',
     ],
     'installable': True,
     'application': False,
